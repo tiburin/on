d387b0a49e82a291212571658ba5ccdc42a00003
Mauricio Barrios <tiburinn@hotmail.com>
2020-05-14 21:51:16 -0700

    able to read the current langs

--- src/v1/store.rs
+++ src/v1/store.rs
@@ -3,6 +3,7 @@
 
 const MAX: usize = 5000;
 use std::collections::HashMap;
+use std::{io, path::Path};
 
 fn parse_content(content: String) -> Vec<(i32, String, String)> {
   content
@@ -29,24 +30,82 @@ fn parse_content(content: String) -> Vec<(i32, String, String)> {
     })
     .collect()
 }
+fn parse_languages(content: String) -> Vec<String> {
+  let list: Vec<String> = content
+    .split("\n")
+    .filter(|n| n != &"")
+    .map(|n| n.trim().to_owned())
+    .collect();
 
-fn start(content: String) {
+  if list.len() < 2 {
+    panic!("languages need to be more than 2 CURRENT {}", list.len())
+  }
+  list
+}
+
+fn start(languages: Vec<String>, data: Vec<(i32, String, String)>) {
+  // let data_box = HashMap::new();
+
   let mut table: HashMap<i32, String> = HashMap::with_capacity(MAX);
-  for (rank, word, sentence) in parse_content(content) {
+  for (rank, word, sentence) in parse_content("..".to_owned()) {
     //TODO
     // insert data into the hashmap
   }
+
+  for language in languages {
+    // TODO insert into hashmap
+  }
+}
+
+pub fn read_languages() -> Result<Vec<String>, io::Error> {
+  let path = Path::new("public/spoken");
+
+  let list = path.read_dir()?.fold(Vec::new(), |mut acc, files| {
+    if let Ok(file) = files {
+      if let Some(os_name) = file.path().file_stem() {
+        os_name.to_str().map(|name| {
+          acc.push(name.trim().to_owned());
+        });
+      }
+    };
+    acc
+  });
+  Ok(list)
 }
 
 #[cfg(test)]
 mod tests {
   use super::*;
-  use std::{fs, panic};
+  use std::panic;
 
   #[test]
+  fn read_languages_okay() {
+    let result = read_languages();
+    assert!(result.unwrap().len() >= 2);
+  }
+  #[test]
   fn start_test() {
-    let content = fs::read_to_string("public/spoken/english.on").unwrap();
-    start(content)
+    // let content = fs::read_to_string("public/spoken/english.on").unwrap();
+    // start(content)
+  }
+
+  #[test]
+  fn parse_languages_errors() {
+    let result = panic::catch_unwind(|| parse_languages(format!("a\n")));
+    assert!(result.is_err());
+
+    let result = panic::catch_unwind(|| parse_languages(format!("")));
+    assert!(result.is_err());
+  }
+  #[test]
+  fn parse_languages_okay() {
+    let result = parse_languages(format!("a\nb\nb"));
+    assert_eq!(result, ["a", "b", "b"]);
+
+    let stage2 = "\nenglish\nespanol\nportugues";
+    let result = parse_languages(format!("a\nb\nz{}", stage2));
+    let response = ["a", "b", "z", "english", "espanol", "portugues"];
+    assert_eq!(result, response);
   }
 
   #[test]
